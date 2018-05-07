dbc99d1c7edcea820ed3fae3afdf7bba6304b092
okee0055 <31739946+okee0055@users.noreply.github.com>
2018-05-06 22:30:48 -0500

    still basic server for game

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 
-var fs =  require('fs');
 var path = require('path');
-var http = require('http');
-var url = require('url');
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
 var port = 8014;
 var public_dir = path.join(__dirname, 'public');
-var assets_dir = path.join(public_dir, 'assets');
 app.get('/', (req, res)=>{
     res.sendFile(path.join(public_dir, 'space.html'));
 });

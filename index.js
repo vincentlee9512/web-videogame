e9a6b56885c71a5dee829ea24ee333e8e37e73d7
okee0055 <31739946+okee0055@users.noreply.github.com>
2018-05-09 21:34:54 -0500

    For the most part stuff works, lost of changes

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ const io = socketIO.listen(server);
 
 const port = 8009;
 
-const ships = [
-    ['yellow', undefined],
-    ['red', undefined],
-    ['purple', undefined],
-    ['green', undefined]
-];
-
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/assets', express.static(__dirname + '/assets'));
@@ -38,15 +31,23 @@ server.listen(port, () => {
 io.on('connection', (socket) => {
     console.log("Client connected");
 
-    socket.on('join_room', (room) => {
+    socket.on('join_room', (player_data) => {
+        let room = player_data.room;
         socket.join(room);
-        io.to(room).emit('player_joined');
-        let id = findOpenID(socket.id);
+        if(typeof io.sockets.adapter.rooms[room].players === 'undefined'){
+          io.sockets.adapter.rooms[room].players = [undefined,undefined,undefined,undefined];
+        }
+        if(typeof io.sockets.adapter.rooms[room].player_ids === 'undefined'){
+          io.sockets.adapter.rooms[room].player_ids = [undefined,undefined,undefined,undefined];
+        }
+        // io.to(room).emit('player_joined');
+        let id = findOpenID(socket.id, io.sockets.adapter.rooms[room].player_ids);
+        console.log(io.sockets.adapter.rooms[room].player_ids);
         socket.emit('assign_id', id);
-
+        io.sockets.adapter.rooms[room].players[id] = {'player_id':id, 'color':player_data.color};
+        io.to(room).emit('add_players', io.sockets.adapter.rooms[room].players);
         let conn_players = io.sockets.adapter.rooms[room].length;
         io.to(room).emit('start_game', conn_players);
-
         socket.on('update', (data) => {
           io.to(room).emit('update_players', data);
         });
@@ -66,15 +67,15 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log("Client disconnected: " + socket.id);
-        removeID(socket.id);
+      console.log("Client disconnected: " + socket.id);
+      removeID(socket.id);
     });
 });
 
-function findOpenID(id) {
-    for (let i = 0; i < ships.length; i++) {
-        if (!ships[i][1]) {
-            ships[i][1] = id;
+function findOpenID(id, player_ids) {
+    for (let i = 0; i < player_ids.length; i++) {
+        if (!player_ids[i]) {
+            player_ids[i] = id;
             return i
         }
     }
@@ -82,18 +83,22 @@ function findOpenID(id) {
 }
 
 function removeID(id) {
-    for (let i = 0; i < ships.length; i++) {
-        if (ships[i][1] === id) {
-            ships[i][1] = undefined;
+  Object.keys(io.sockets.adapter.rooms).forEach((room) => {
+    if(typeof io.sockets.adapter.rooms[room].player_ids !== 'undefined'){
+      for (let i = 0; i < io.sockets.adapter.rooms[room].player_ids.length; i++) {
+        if (io.sockets.adapter.rooms[room].player_ids[i] === id) {
+            io.sockets.adapter.rooms[room].player_ids[i] = undefined;
+            io.to(room).emit('player_disconnect');
             return;
         }
+      }
     }
+  });
 }
 
 
 function getAllPlayers() {
     const players = [];
-
     Object.keys(io.sockets.connected).forEach((socketID) => {
         let player = io.sockets.connected[socketID].player;
         if (player) players.push(player);
--- js/game.js
+++ js/game.js
@@ -12,30 +12,25 @@ const config = {
             debug: false
         }
     },
-    // scene: {
-    //     preload: preload,
-    //     create: create,
-    //     update: update
-    // }
+    parent: 'gameDiv',
     scene: gameScene
 };
 
-let players;
-let player_count = 4;
-const player_init = [
-                      {'yellow': [100, 300]},
-                      {'yellow': [100, 300], 'red': [1100, 300]},
-                      {'yellow': [100, 150], 'red': [1100, 300], 'purple': [100, 450]},
-                      {'yellow': [100, 150], 'red': [1100, 150], 'purple': [100, 450], 'green': [1100, 450]}
-                    ];
-// const player_init = {'yellow': [100, 150], 'red': [1100, 150], 'purple': [100, 450], 'green': [1100, 450]};
+let players = [undefined, undefined, undefined, undefined];
+let players_grp;
+const spawn_points = [
+                        [[100, 300]],
+                        [[100, 300], [1100, 300]],
+                        [[100, 150], [1100, 300], [100, 450]],
+                        [[100, 150], [1100, 150], [100, 450], [1100, 450]]
+                      ];
 let lazers;
 let player_speed = -400;
 let lazer_vel = 600;
 let cursors;
 const game_over = false;
 //Server emits to set this to true once everyone has entered the game
-// let game_start = true;
+let game_start = false;
 
 const game = new Phaser.Game(config);
 
@@ -46,6 +41,9 @@ gameScene.preload = function(){
     this.load.image('green', '/assets/ships/green.png');
     this.load.image('red', '/assets/ships/red.png');
     this.load.image('purple', '/assets/ships/purple.png');
+    this.load.image('black', '/assets/ships/black.png');
+    this.load.image('blue', '/assets/ships/blue.png');
+    this.load.image('orange', '/assets/ships/orange.png');
     this.load.image('lazer', '/assets/lazer.png')
 }
 
@@ -54,29 +52,24 @@ gameScene.create = function(){
     // background for our game
     this.add.image(600, 300, 'background');
 
-    // player ship
+    // players_grp for collider, and adding ships
     //--------------------------------------------------------------------------
-    players = this.physics.add.group();
-    Object.keys(player_init[player_count-1]).forEach((ship) => {
-        const player = players.create(player_init[player_count-1][ship][0], player_init[player_count-1][ship][1], ship);
-        player.angle = (player_init[player_count-1][ship][0] > 600) ? 180 : 0;
-        player.setCollideWorldBounds(true);
-        player.body.immovable = true;
-        player.weapon_loaded = true;
-    });
-    console.log(player_count);
-    console.log(players);
+    players_grp = this.physics.add.group();
+    // console.log(players_grp);
     //--------------------------------------------------------------------------
 
     // lazers group/collider
     //--------------------------------------------------------------------------
     lazers = this.physics.add.group();
-    this.physics.add.collider(players, lazers, player_destroy, null, this);
+    this.physics.add.collider(players_grp, lazers, player_destroy, null, this);
     //--------------------------------------------------------------------------
 
     // Input Events
     cursors = this.input.keyboard.createCursorKeys();
 
+    // had to emit here so that that it would not try to
+    // create ships until the game had been created
+    socket.emit('join_room', {'room':room, 'color':ship_color});
 }
 
 
@@ -85,12 +78,14 @@ gameScene.update = function(){
     if (game_over) {
         return;
     }
-    player_controller(players.children.entries[player_id]);
+    if(typeof players[player_id] !== 'undefined'){
+      player_controller(players[player_id]);
+    }
 }
 
 function player_controller(player) {
 
-  // if(game_start){
+  if(game_start){
     //vertical movement
     if (cursors.up.isDown) {
         player.setVelocityY(player_speed);
@@ -121,31 +116,22 @@ function player_controller(player) {
 
     // Shooting
     if (player.weapon_loaded && player.active && cursors.space.isDown) {
-        //update.hasShot = true;
         shoot();
-        // gameScene.scene.start();
     }
     socket.emit('update', update);
-  // }
+  }
 }
 
 socket.on('start_game', (num_connected) => {
-  player_count = num_connected;
-  console.log("startGame!");
-  // console.log(player_count);
-  //game_start = true;
-  // game.scene.restart();
-  // window.location.reload();
+  if(num_connected === player_count){
+    game_start = true;
+  }
 });
 
 socket.on('update_players', (data) => {
   if(typeof players !== 'undefined'){
     if (data.id !== player_id) {
-      let player = players.children.entries[data.id];
-      // if (data.hasShot && player.weapon_loaded && player.active && cursors.space.isDown) {
-      //   update.hasShot = true;
-      //   shoot(player);
-      // }
+      let player = players[data.id];
       player.x = data.x;
       player.y = data.y;
     }
@@ -153,17 +139,17 @@ socket.on('update_players', (data) => {
 });
 
 socket.on('player_destroy', (data) => {
-  players.children.entries[data].disableBody(true, true);
+  players[data].disableBody(true, true);
 
 });
 
 socket.on('player_respawn', (data) => {
-  ship = players.children.entries[data].texture.key;
-  players.children.entries[data].enableBody(true, player_init[player_count-1][ship][0], player_init[player_count-1][ship][1], true, true);
+  ship = players[data].texture.key;
+  players[data].enableBody(true, spawn_points[player_count-1][player_id][0], spawn_points[player_count-1][player_id][1], true, true);
 });
 
 socket.on('player_shot', (data) => {
-  let ship = players.children.entries[data];
+  let ship = players[data];
   const lz = (ship.angle === 0) ? {'x': ship.x + 40, 'vel': lazer_vel} : {'x': ship.x - 40, 'vel': -lazer_vel};
   const lazer = lazers.create(lz['x'], ship.y, 'lazer');
   lazer.setCollideWorldBounds(false);
@@ -175,28 +161,55 @@ socket.on('player_shot', (data) => {
   }, 1500);
 });
 
-function enemy_shoot() {
-    enemies.children.entries.forEach(function (enemy) {
-        setInterval(() => {
-            if (enemy.weapon_loaded && enemy.active) {
-                shoot(enemy);
-            }
-        }, 1000 * Phaser.Math.RND.integerInRange(1, 5))
-    });
+socket.on('add_players', (data) => {
+  console.log('Trying to add player(s)');
+  console.log(data);
+  data.forEach((cur_player) => {
+    console.log(cur_player);
+    if(cur_player){
+      let should_create = true;
+      players.forEach((player) => {
+        console.log(player);
+        if(player){
+          if(player.player_id === cur_player.player_id){
+            should_create = false;
+            console.log('player already exists');
+          }
+        }else if(cur_player.player_id > player_count-1){
+          should_create = false;
+          console.log('Invalid ID: ' + cur_player.player_id);
+        }
+      });
+      if(should_create){
+        gameScene.createPlayer(cur_player);
+      }
+    }
+  });
+});
+
+gameScene.createPlayer = function(new_player){
+  let player = players_grp.create(spawn_points[player_count-1][new_player.player_id][0], spawn_points[player_count-1][new_player.player_id][1], new_player.color);
+  player.angle = (spawn_points[player_count-1][new_player.player_id][0] > 600) ? 180 : 0;
+  player.setCollideWorldBounds(true);
+  player.body.immovable = true;
+  player.weapon_loaded = true;
+  player.player_id = new_player.player_id;
+  player.color = new_player.color;
+  players[new_player.player_id] = player;
 }
 
 function shoot() {
   socket.emit('shoot', player_id);
-    players.children.entries[player_id].weapon_loaded = false;
+    players[player_id].weapon_loaded = false;
     setTimeout(() => {
-        players.children.entries[player_id].weapon_loaded = true;
+        players[player_id].weapon_loaded = true;
     }, 500);
 }
 
 
 function player_destroy(player, lazer) {
   player.disableBody(true, true);
-  let player_is_me = player.texture.key === players.children.entries[player_id].texture.key;
+  let player_is_me = player.texture.key === players[player_id].texture.key;
   if(player_is_me){
     socket.emit('player_hit', player_id);
     setTimeout(() => {
